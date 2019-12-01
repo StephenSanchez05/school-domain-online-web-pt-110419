8d79b92c469a5816083f09c331849bae783aace0
Stephen Sanchez <stephensanchez05@gmail.com>
2019-12-01 02:01:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,13 @@
 class School
   attr_accessor :name
   
-  ROSTER ||= {"key" => "value"}
-  
   def initialize(name)
     @name = name
+    @roster = {}
   end
   
   
   def add_student(name, grade)
-    ROSTER["#{grade}"] = "#{name}"
+    @roster["#{grade}"] = "#{name}"
   end
 end
