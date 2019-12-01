b02cbe1e83cb5fdffa810cc053cf16ab7635b85f
Stephen Sanchez <stephensanchez05@gmail.com>
2019-12-01 02:17:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
   def add_student(name, grade)
     @roster ||= @roster[grade] = "#{name}"
     if @roster.include?(grade)
-      @roster[grade] << ", #{name}"
+      @roster[grade] << "#{name}"
     else @roster[grade] = ["#{name}"]
     end
   end
